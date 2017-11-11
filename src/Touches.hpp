7962b8198150f666ddc97043c077bf6b3de61638
Laurent MALKA <laurent.m512@gmail.com>
2017-11-11 12:34:12 +0100

    Touches

--- src/Touches.hpp
+++ src/Touches.hpp
@@ -2,7 +2,35 @@
 #define MJAM2017_TOUCHES_HPP
 
 
+#include <SDL2/SDL_scancode.h>
+
 class Touches {
+public:
+    Touches(SDL_Scancode t_haut, SDL_Scancode t_bas, SDL_Scancode t_gauche, SDL_Scancode t_droite, SDL_Scancode t_saut,
+            SDL_Scancode t_grappin, SDL_Scancode t_bonus);
+
+    SDL_Scancode getT_haut() const;
+
+    SDL_Scancode getT_bas() const;
+
+    SDL_Scancode getT_gauche() const;
+
+    SDL_Scancode getT_droite() const;
+
+    SDL_Scancode getT_saut() const;
+
+    SDL_Scancode getT_grappin() const;
+
+    SDL_Scancode getT_bonus() const;
+
+private:
+    SDL_Scancode t_haut;
+    SDL_Scancode t_bas;
+    SDL_Scancode t_gauche;
+    SDL_Scancode t_droite;
+    SDL_Scancode t_saut;
+    SDL_Scancode t_grappin;
+    SDL_Scancode t_bonus;
 
 };
 
