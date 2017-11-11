f38cc73e13bf72a431c3451521dfa5bae1ea9c3a
Laurent MALKA <laurent.m512@gmail.com>
2017-11-11 16:54:56 +0100

    *

--- src/Moteur.hpp
+++ src/Moteur.hpp
@@ -1,10 +1,15 @@
 #ifndef MJAM2017_MOTEUR_HPP
 #define MJAM2017_MOTEUR_HPP
 
+#include "Inputs.hpp"
+
 class Moteur {
 public:
     Moteur();
     void deplacements();
+
+private:
+    Inputs inputs;
 };
 
 
