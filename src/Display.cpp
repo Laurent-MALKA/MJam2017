6a50b5cd8f33f1cc13a4c28c9432666ba2fb3e1e
alpeng <alxpg@hotmail.fr>
2017-11-12 05:51:18 +0100

    Display: correction

--- src/Display.cpp
+++ src/Display.cpp
@@ -99,8 +99,6 @@ bool Display::isInitialized() {
 
 
 void Display::display(Perso *p1, Perso *p2,Map map, int checkpoints[NB_CHECKPOINTS][2]) {
-   double d1 = Body(checkpoints[0][0],checkpoints[0][1],0,0).distance(*p1);
-   double d2 = Body(checkpoints[0][0],checkpoints[0][1],0,0).distance(*p2);
    SDL_SetRenderDrawColor(rdr,50,50,50,0);
    SDL_RenderClear(rdr);
    map.display(rdr,map_rect);
